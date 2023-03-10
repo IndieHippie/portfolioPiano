83a15b266a753b64adbc446fe96ec366724d01eb
RomanStets <roma22stets@gmail.com>
2023-03-10 17:31:30 +0200

    main changed

--- components/Main.jsx
+++ components/Main.jsx
@@ -10,19 +10,18 @@ const Main = () => {
       <div className='max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center'>
         <div>
           <p className='uppercase text-sm tracking-widest text-gray-600'>
-            LET&#39;S BUILD SOMETHING TOGETHER
+            LET&#39;S PLAY SOMETHING TOGETHER
           </p>
           <h1 className='py-4 text-gray-700'>
-            Hi, I&#39;m <span className='text-[#5651e5]'> Clint</span>
+            Hi, I&#39;m <span className='text-[#429599]'> Roman</span>
           </h1>
-          <h1 className='py-2 text-gray-700'>A Front-End Web Developer</h1>
+          <h1 className='py-2 text-gray-700'>Professional Piano Player <br></br> Piano Teacher <br></br> Concertmaster</h1>
           <p className='py-4 text-gray-600 sm:max-w-[70%] m-auto'>
-            I’m focused on building responsive front-end web applications
-            integrating back-end technologies.
+            I'm focused on helping students develop their skills and achieve their musical goals.
           </p>
           <div className='flex items-center justify-between max-w-[330px] m-auto py-4'>
             <a
-              href='https://www.linkedin.com/in/clint-briley-50056920a/'
+              href='https://www.linkedin.com/in/roman-stets-ab48bb254/'
               target='_blank'
               rel='noreferrer'
             >
@@ -31,7 +30,7 @@ const Main = () => {
               </div>
             </a>
             <a
-              href='https://github.com/fireclint'
+              href='https://github.com/IndieHippie'
               target='_blank'
               rel='noreferrer'
             >
