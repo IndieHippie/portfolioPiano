3c7ae5ccecf02f91304c5f2697f149a143954ba7
RomanStets <roma22stets@gmail.com>
2023-03-10 19:22:47 +0200

    skills                 rel="noreferrer"

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -11,7 +11,7 @@ const Projects = () => {
         <p className="text-xl tracking-widest uppercase text-[#429599]">
           Demo
         </p>
-        <h2 className="py-4">What I've Built</h2>
+        <h2 className="py-4">What I&#39;ve Built</h2>
         <div className="grid md:grid-cols-2 gap-8">
           <div className="relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl p-4 group hover:bg-gradient-to-r from-[#429599] to-[#a3cacc]">
             <Image
@@ -21,13 +21,14 @@ const Projects = () => {
             />
             <div className="hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
               <h3 className="text-2xl text-white tracking-wider text-center">
-                Puccini – 'Che gelida manina'
+                Puccini – &#39;Che gelida manina&#39;
               </h3>
               <p className="pb-4 pt-2 text-white text-center">
                 Tenore - Yu Yang, Concertmaster - Roman Stets
               </p>
               <a
                 target="_blank"
+                rel="noreferrer"
                 href="https://www.youtube.com/watch?v=k1Mk2m-5X7k"
               >
                 <p className="text-center p-3 rounded-lg bg-white text-gray-700 font-bold text-lg cursor-pointer">
@@ -51,6 +52,7 @@ const Projects = () => {
               </p>
               <a
                 target="_blank"
+                rel="noreferrer"
                 href="https://www.youtube.com/watch?v=feZi-lkHfz8"
               >
                 <p className="text-center p-3 rounded-lg bg-white text-gray-700 font-bold text-lg cursor-pointer">
